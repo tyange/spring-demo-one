d1e30f12b2de0098da83735ee4c83436495477b5
babyazalea <usun12@hotmail.com>
2021-11-09 15:45:00 +0900

    added CricketCoach

---
+++ src/com/luv2code/springdemo/CricketCoach.java
@@ -0,0 +1,27 @@
+package com.luv2code.springdemo;
+
+public class CricketCoach implements Coach {
+
+    private FortuneService fortuneService;
+
+    // create a no-arg constructor
+    public CricketCoach() {
+        System.out.println("CricketCoach: inside no-arg constructor");
+    }
+
+    // our setter method
+    public void setFortuneService(FortuneService fortuneService) {
+        System.out.println("CricketCoach: inside setter method");
+        this.fortuneService = fortuneService;
+    }
+
+    @Override
+    public String getDailyWorkout() {
+        return "Practice fast bowling for 15 min.";
+    }
+
+    @Override
+    public String getDailyFortune() {
+        return fortuneService.getFortune();
+    }
+}
